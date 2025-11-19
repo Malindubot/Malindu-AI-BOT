c3a87061d2527ef97c1208cac19106b80dd3667e
Malindubot <malindunadith52@gmail.com>
2025-11-19 17:51:30 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
-ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DANUWA-MD Is Alive Now😍*",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/Malindubot/Malindu-AI-BOT/blob/main/images/Gemini_Generated_Image_b2vl32b2vl32b2vl.png?raw=true",
+ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 Malindu AI BOT Is Alive Now😍*",
 BOT_OWNER: '94776121326',  // Replace with the owner's phone number
 
 
